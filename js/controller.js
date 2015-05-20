3fb79dea135b188ec268eb571408c1e40eab4074
Alix <ducrosalix@hotmail.fr>
2015-05-20 19:56:23 +0200

    amélioré formulaire de recherche et presentation des résultats

--- js/controller.js
+++ js/controller.js
@@ -5,24 +5,26 @@ angular.module('app').controller("SearchController", function(){
     viewModel.searchInputStart = '';
     viewModel.searchInputDestination = '';
     viewModel.searchInputDate = '';
+	
+	viewModel.matchingTrajets = [];
     
     viewModel.trajets = [
 		{
 			depart : "Clermont-Ferrand",
 			destination : "Paris",
-			date : "2015-06-06",
+			date : new Date("2015-06-06"),
 			chauffeur : "Bobby F."
 		},
 		{
 			depart : "Strasbourg",
 			destination : "Nice",
-			date : "2015-05-25",
+			date : new Date("2015-05-25"),
 			chauffeur : "Jackie M."
 		},
 		{
 			depart : "Lyon",
 			destination : "Brest",
-			date : "2015-06-01",
+			date : new Date("2015-06-01"),
 			chauffeur : "Marie O. D."
 		}
 	];
@@ -30,19 +32,45 @@ angular.module('app').controller("SearchController", function(){
 	viewModel.ordersList = [
         {
             id: 1,
-            title: 'Date Ascending',
+            title: 'Date ascendante',
             key: 'date',
             reverse: false
         },
         {
             id: 2,
-            title: 'Date Descending',
+            title: 'Date descendante',
             key: 'date',
             reverse: true
         }
     ];
     viewModel.order = viewModel.ordersList[0];
-    
+	
+	viewModel.fillMatchingTrajets = function () {
+		//On vide le tableau de résultats
+		viewModel.matchingTrajets.length = 0 ;
+		
+		viewModel.trajets.forEach(function(trajet) {
+			//On vérifie le départ
+			var departMatched = false;
+			var destinationMatched = false;
+			
+			if(trajet.depart.toUpperCase().indexOf(viewModel.searchInputStart.toUpperCase()) != -1) {
+				departMatched = true ;
+			}
+			
+			if(trajet.destination.toUpperCase().indexOf(viewModel.searchInputDestination.toUpperCase()) != -1) {
+				destinationMatched = true ;
+			}
+			
+			if(departMatched && destinationMatched) {
+				viewModel.matchingTrajets.push(trajet);
+				console.log("Matched !");
+			}
+			
+		}, this);
+		
+		
+	}    
 });
 
 
